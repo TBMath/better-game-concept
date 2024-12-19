1299ca9ce4dc5f2531fb417f89e5dd11d1bdd897
Tirtha Biswas <tirthabiswasm@gmail.com>
2024-12-19 22:15:11 +1100

    i think i did it

--- scripts/start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Navigate to your project directory and then to the backendcd /home/ec2-user/better-game-concept/backend
-
-# Restart the app using pm2
-pm2 restart "my-backend" || pm2 start server.js --name "my-backend" --watch
--- scripts/stop_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Ensure that any previous server instances are stopped.
-
-pm2 stop "my-backend" || true
