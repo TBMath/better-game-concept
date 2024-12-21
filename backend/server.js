f2c33277dc3c0bce375283e174fdb32f68cd66f7
Tirtha Biswas <tirthabiswasm@gmail.com>
2024-12-22 00:05:38 +1100

    trying

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,6 @@ app.use('/api', router);
 app.get('/api', (req, res) => {res.json({ message: 'You have connected to the API' });});
 
 app.use(cors());
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Backend running at http://0.0.0.0:${port}`);
+app.listen(port, () => {
+  console.log(`Backend running at http://localhost:${port}`);
 });
