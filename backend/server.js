f5110557adf94b3a8e04c2d91a8ae2c2d3ba9951
Tirtha Biswas <tirthabiswasm@gmail.com>
2024-12-22 13:05:23 +1100

    fix: clean up deployment workflow and server.js formatting

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ app.use('/api', router);
 
 app.get('/api', (_, res) => {res.json({ message: 'You have connected to the API' });});
 
-app.use(cors());
-app.listen(port,'0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Backend running at http://localhost:${port}`);
 });
