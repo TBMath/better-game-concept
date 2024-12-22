ecc9ae29f043a471f664fcfd273e6ddcda1c82e2
Tirtha Biswas <tirthabiswasm@gmail.com>
2024-12-22 12:45:54 +1100

    update server port to 5000 and remove Nginx reload command

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import logRequest from "./middleware/logger.js";
 import router from "./routes/index.js";
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Enable CORS for the frontend
 app.use(cors());
