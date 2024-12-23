6e464f3869be0ea88399b63677e259b53f974404
Tirtha Biswas <tirthabiswasm@gmail.com>
2024-12-23 11:17:11 +1100

    Update mongodb.js

--- backend/utils/mongodb.js
+++ backend/utils/mongodb.js
@@ -1,12 +1,7 @@
 import { MongoClient } from 'mongodb';
 const uri = "mongodb+srv://tirthabiswasm:hx5UAIgjSOcxrbus@cluster0.mbjn2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const options = {
-    ssl: true,
-    tls: true,
-    tlsAllowInvalidCertificates: false,
-    useUnifiedTopology: true,
-  };
-const client = new MongoClient(uri,options);
+
+const client = new MongoClient(uri);
 const clientPromise = client.connect();
 
 export default clientPromise;
